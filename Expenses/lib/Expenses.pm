283279828a9558b0cef96ac056ebe974db9125ba
brandon <brawer@brawer.dev>
2021-10-02 14:42:46 -0400

    Sets up database and sessions
    * User login added

--- Expenses/lib/Expenses.pm
+++ Expenses/lib/Expenses.pm
@@ -1,10 +1,56 @@
 package Expenses;
 use Dancer2;
 
+use Models::Database;
+use Models::User;
+use Time::HiRes qw( time );
+use Module::Runtime 'require_module';
+
+my $db = Models::Database->new();
+
+use Controllers::UserController;
+use Controllers::MainController;
+
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index' => { 'title' => 'Expenses' };
+my @times;
+my $start_time;
+my $finish_time;
+
+hook before => sub
+{
+    $start_time = time();
 };
 
-true;
+hook after => sub
+{
+    $finish_time = time();
+
+    my $delta = ( $finish_time - $start_time );
+
+    print "\n\nDelta this run is: $delta seconds\n\n";
+    push @times, $delta;
+
+    if ( scalar @times > 1 )
+    {
+        my $total;
+        my $count = scalar @times;
+        grep { $total += $_ } @times;
+
+        my $average = ( $total / $count );
+
+        print "\n\nAverage of $count is $average seconds\n\n";
+    }
+};
+
+any qr{.*} => sub
+{
+    my $user       = session('user') // Models::User->new();
+    template 'special_404',
+      {
+        'path'       => request->path,
+        'logged_in'  => $user->logged_in
+      };
+};
+
+1;
