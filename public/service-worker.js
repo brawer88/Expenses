c9665d9fd9cd97f607340a366992c8971b77af15
brandon <brawer@brawer.dev>
2021-10-12 17:59:33 -0400

    fixes manifest

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+
+// This will trigger the importScripts() for workbox.strategies and its dependencies:
+workbox.loadModule('workbox-strategies');
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('.js')) {
+    // Referencing workbox.strategies will now work as expected.
+    const cacheFirst = new workbox.strategies.CacheFirst();
+    event.respondWith(cacheFirst.handle({request: event.request}));
+  }
+});
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
