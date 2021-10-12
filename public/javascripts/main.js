8e0ed56fc4d2ac9f09685c3c38a4b7003d062745
brandon <brawer@brawer.dev>
2021-10-12 19:05:58 -0400

    empties fethc handler...

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,3 +11,27 @@ function getBanks(){
         $("#bank_div").html(data["text"]);
     });
 }
+
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' });
+  });
+}
+
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
